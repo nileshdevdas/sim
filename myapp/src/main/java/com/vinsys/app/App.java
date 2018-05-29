be8b5db9735997a7a040ac3b040d01a725552a01
DESKTOP-1L38NVA\Nilesh Devdas <nilesh.devdas@gmail.com>
2018-05-29 20:18:05 +0530

    hello2.html

---
+++ myapp/src/main/java/com/vinsys/app/App.java
@@ -0,0 +1,13 @@
+package com.vinsys.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
